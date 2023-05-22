d7063dd60143b1db568d0d61af7cebac0c2d8b44
chan_m <>
2023-05-23 01:58:21 +0900

    以下の機能追加。 index.js ・メッセージ追加 ・毎週金曜日の18時にメッセージにて催促。

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 //dotenvの適用
 require('dotenv').config();
-//.envからtokenの呼び出し
-const {token} = process.env;
+
+//cronの適用
+const CronJob = require('cron').CronJob;
+
+//.envからの呼び出し
+const token = process.env.DISCORD_TOKEN;
+const cookChannel = process.env.COOK_CHANNEL;
 
 const { Client, GatewayIntentBits } = require('discord.js');
 
@@ -28,6 +33,21 @@ client.on('messageCreate', message => {
   if(message.content === "おい") {
     message.channel.send("だいすーしーキッチンオープンして");
   }
+  if(message.content === "は？") {
+    message.channel.send("やれよ");
+  }
+});
+
+client.on('ready', async () => {
+  //チャンネル指定
+  const channel = await client.channels.fetch(cookChannel);
+
+  //毎週金曜日 18:00に催促メッセージを送信
+  new CronJob('00 00 18 * * Friday', () => {
+      //処理
+      channel.send('だいすーしーキッチンオープンして');
+  }, null, true, 'Asia/Tokyo');
+
 });
 
 client.on('ready', () => {
